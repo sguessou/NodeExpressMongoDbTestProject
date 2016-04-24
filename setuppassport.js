420bb5bce2916006d09733e413d12f35dddc90dd
Saad Guessous <guessous_saad@hotmail.com>
2016-04-24 18:39:06 +0300

    passport local strategy

--- setuppassport.js
+++ setuppassport.js
@@ -1,6 +1,24 @@
 var passport = require("passport");
+var LocalStrategy = require("passport-local").Strategy;
 
 var User = require("./models/user");
+
+passport.use("login", new LocalStrategy(function (username, password, done) {
+	User.findOne({ username: username }, function (err, user) {
+		if (err) { return done(err); }
+		if (!user) { 
+			return done(null, false, { message: "No user has that username!" });
+		}
+		user.checkPassword(password, function (err, isMatch) {
+			if (err) { return done(err); }
+			if (isMatch) {
+				return done(null, user);
+			} else {
+				return done(null, false, { message: "Invalid password." });
+			}
+		});
+	});
+}));
 
 module.exports = function () {
 	passport.serializeUser(function (user, done) {
