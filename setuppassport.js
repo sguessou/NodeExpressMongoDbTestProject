d4ab056b473f1f034d92e697ab66fc05b1491251
Saad Guessous <guessous_saad@hotmail.com>
2016-04-24 18:25:38 +0300

    passport serialize/deserialize function

---
+++ setuppassport.js
@@ -0,0 +1,15 @@
+var passport = require("passport");
+
+var User = require("./models/user");
+
+module.exports = function () {
+	passport.serializeUser(function (user, done) {
+		done(null, user._id);
+	});
+
+	passport.deserializeUser(function (id, done) {
+		User.findById(id, function (err, user) {
+			done(err, user);
+		});
+	});
+};
